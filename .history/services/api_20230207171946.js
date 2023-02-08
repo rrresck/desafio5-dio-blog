7065e7957e9ead08fcfc15847ca98de2deea8af0
Rafael Rodrigues Resck <rrresck@gmail.com>
2023-02-07 21:52:58 -0300

    Entrega do desafio 5, do Curso Formação React Developer da DIO.

---
+++ .history/services/api_20230207171946.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const api = axios.create({
+    baseURL: 'https://fpbffjbhosvcjhcrxdxt.supabase.co/rest/v1/',
+    headers: {
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwYmZmamJob3N2Y2poY3J4ZHh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzU3OTczOTAsImV4cCI6MTk5MTM3MzM5MH0.vIkuQYhX7vrDUEcrbxMMxFkIonPumROtzfdff25pLO8",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwYmZmamJob3N2Y2poY3J4ZHh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzU3OTczOTAsImV4cCI6MTk5MTM3MzM5MH0.vIkuQYhX7vrDUEcrbxMMxFkIonPumROtzfdff25pLO8"
+    }
+})
